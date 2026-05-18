ed5f1c5f092cf93c263334868a19338c96e4b25b
Xander Alec White <xaw8720@rit.edu>
2026-05-18 19:34:15 +0000

    Setup Java backend

---
+++ server/src/main/java/com/schedulehelper/api/schedulehelperapi/ScheduleHelperApiApplication.java
@@ -0,0 +1,13 @@
+package com.schedulehelper.api.schedulehelperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleHelperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleHelperApiApplication.class, args);
+	}
+
+}
