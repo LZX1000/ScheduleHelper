133a3ee829e50728a293cb7d49dcadf5ca75c9ae
Xander White <xaw8720@g.rit.edu>
2026-05-19 09:14:04 -0400

    feat: add employee JPA entity based on current SQL

---
+++ server/src/main/java/com/schedulehelper/api/schedulehelperapi/entity/Employee.java
@@ -0,0 +1,48 @@
+package com.schedulehelper.api.schedulehelperapi.entity;
+
+import jakarta.persistence.*;
+
+
+/**
+ * Entity representing the {@code employee} table.
+ *
+ * @see <a href="db/schedulehelper.sql">schedulehelper.sql</a>
+ */
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    /**
+     * Auto-generated primary key.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * Employee's first name. Required.
+     */
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    /**
+     * Employee's last name. Optional.
+     */
+    @Column(name = "last_name")
+    private String lastName;
+
+    // CONSTRUCTOR
+
+    public Employee() {}
+
+    // GETTERS
+
+    public Integer getId() { return this.id; }
+    public String getFirstName() { return this.firstName; }
+    public String getLastName() { return this.lastName; }
+
+    // SETTERS
+
+    public void setFirstName(final String newFirstName) { this.firstName = newFirstName; }
+    public void setLastName(final String newLastName) { this.lastName = newLastName; }
+}
