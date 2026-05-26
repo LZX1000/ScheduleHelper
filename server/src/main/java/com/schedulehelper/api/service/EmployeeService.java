a7293c7c99a32692fca2628ed55256c5fa778bbe
Xander White <xaw8720@g.rit.edu>
2026-05-26 11:42:54 -0400

    feat: add info logging for state change and warn loggings for exceptions

--- server/src/main/java/com/schedulehelper/api/service/EmployeeService.java
+++ server/src/main/java/com/schedulehelper/api/service/EmployeeService.java
@@ -3,6 +3,8 @@ package com.schedulehelper.api.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +15,9 @@ import com.schedulehelper.api.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
-    final EmployeeRepository employeeRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeService.class);
+
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
     public EmployeeService(final EmployeeRepository employeeRepository) {
@@ -22,21 +26,30 @@ public class EmployeeService {
 
     @Transactional
     public void createNew(final Employee employee) {
-        if (employee.getId() != null) {
+        final Integer employeeId = employee.getId();
+
+        if (employeeId != null) {
+            LOG.warn("Attempted to create employee with defined id {}", employeeId);
             throw new IllegalArgumentException("New employee must not have an ID.");
         }
+
         this.employeeRepository.save(employee);
+        LOG.info("Created new employee with id {}", employeeId);
     }
 
     @Transactional
     public void updateById(final Employee employee) {
         final Integer employeeId = employee.getId();
+        
         if (employeeId == null) {
+            LOG.warn("Attempted to update employee with null id");
             throw new IllegalArgumentException("Employee must have an ID to be updated.");
         }
         if (!this.employeeRepository.existsById(employeeId)) {
+            LOG.warn("Attempted to update non-existent employee with id {}", employeeId);
             throw new EmployeeNotFoundException(employeeId);
         }
+
         this.employeeRepository.save(employee);
     }
 
@@ -50,11 +63,14 @@ public class EmployeeService {
     }
 
     @Transactional
-    public void deleteById(final Integer id) {
-        if (!this.employeeRepository.existsById(id)) {
-            throw new EmployeeNotFoundException(id);
+    public void deleteById(final Integer employeeId) {
+        if (!this.employeeRepository.existsById(employeeId)) {
+            LOG.warn("Attempted to delete non-existent employee with id {}", employeeId);
+            throw new EmployeeNotFoundException(employeeId);
         }
-        this.employeeRepository.deleteById(id);
+        
+        this.employeeRepository.deleteById(employeeId);
+        LOG.info("Deleted employee with id {}", employeeId);
     }
 
     @Transactional(readOnly = true)
