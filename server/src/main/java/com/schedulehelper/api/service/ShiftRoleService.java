90a357f3827b9d56fc51fc15be6ee0ce470a474a
Xander White <xaw8720@g.rit.edu>
2026-05-26 14:37:47 -0400

    feat: add initial ShiftRole service

---
+++ server/src/main/java/com/schedulehelper/api/service/ShiftRoleService.java
@@ -0,0 +1,71 @@
+package com.schedulehelper.api.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.schedulehelper.api.entity.ShiftRole;
+import com.schedulehelper.api.exception.ShiftNotFoundException;
+import com.schedulehelper.api.repository.ShiftRoleRepository;
+
+@Service
+public class ShiftRoleService {
+    private static final Logger LOG = LoggerFactory.getLogger(ShiftRoleService.class);
+
+    private final ShiftRoleRepository shiftRoleRepository;
+
+    @Autowired
+    public ShiftRoleService(final ShiftRoleRepository shiftRoleRepository) {
+        this.shiftRoleRepository = shiftRoleRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public ShiftRole findById(final Integer id) {
+        return this.shiftRoleRepository.findById(id)
+            .orElseThrow(() -> new ShiftNotFoundException(id));
+    }
+
+    @Transactional
+    public void createNew(final ShiftRole shiftRole) {
+        final Integer shiftRoleId = shiftRole.getId();
+
+        if (shiftRoleId != null) {
+            LOG.warn("Attempted to create shift_role with predefined id {}", shiftRoleId);
+            throw new IllegalArgumentException("New ShiftRole must not have an ID.");
+        }
+
+        final ShiftRole savedShiftRole = this.shiftRoleRepository.save(shiftRole);
+        LOG.info("Created new shift with id {}", savedShiftRole.getId());
+    }
+
+
+    @Transactional
+    public void deleteById(final Integer shiftRoleId) {
+        if (!this.shiftRoleRepository.existsById(shiftRoleId)) {
+            LOG.warn("Attempted to delete non-existent shift_role with id {}", shiftRoleId);
+            throw new ShiftNotFoundException(shiftRoleId);
+        }
+        
+        this.shiftRoleRepository.deleteById(shiftRoleId);
+        LOG.info("Deleted shift with id {}", shiftRoleId);
+    }
+
+    @Transactional
+    public void updateById(final ShiftRole shiftRole) {
+        final Integer shiftRoleId = shiftRole.getId();
+        
+        if (shiftRoleId == null) {
+            LOG.warn("Attempted to update shift_role with null id");
+            throw new IllegalArgumentException("ShiftRole must have an ID to be updated.");
+        }
+        if (!this.shiftRoleRepository.existsById(shiftRoleId)) {
+            LOG.warn("Attempted to update non-existent shift_role with id {}", shiftRoleId);
+            throw new ShiftNotFoundException(shiftRoleId);
+        }
+
+        this.shiftRoleRepository.save(shiftRole);
+        LOG.info("Updated shift with id {}", shiftRoleId);
+    }
+}
