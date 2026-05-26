89fa0541507cfa70814e2b1ed9af989fbb4b6df0
Xander White <xaw8720@g.rit.edu>
2026-05-26 11:27:47 -0400

    feat: add transactional and autowired decorators

--- server/src/main/java/com/schedulehelper/api/service/EmployeeService.java
+++ server/src/main/java/com/schedulehelper/api/service/EmployeeService.java
@@ -3,7 +3,9 @@ package com.schedulehelper.api.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.schedulehelper.api.entity.Employee;
 import com.schedulehelper.api.exception.EmployeeNotFoundException;
@@ -13,10 +15,12 @@ import com.schedulehelper.api.repository.EmployeeRepository;
 public class EmployeeService {
     final EmployeeRepository employeeRepository;
 
+    @Autowired
     public EmployeeService(final EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
+    @Transactional
     public void createNew(final Employee employee) {
         if (employee.getId() != null) {
             throw new IllegalArgumentException("New employee must not have an ID.");
@@ -24,6 +28,7 @@ public class EmployeeService {
         this.employeeRepository.save(employee);
     }
 
+    @Transactional(readOnly = true)
     public List<Employee> findByPartialName(
         final Optional<String> first, final Optional<String> last
     ) {
@@ -32,6 +37,7 @@ public class EmployeeService {
         return this.employeeRepository.findByName(first.orElse(null), last.orElse(null));
     }
 
+    @Transactional
     public void deleteById(final Integer id) {
         if (!this.employeeRepository.existsById(id)) {
             throw new EmployeeNotFoundException(id);
@@ -39,6 +45,7 @@ public class EmployeeService {
         this.employeeRepository.deleteById(id);
     }
 
+    @Transactional(readOnly = true)
     public Employee findById(final Integer id) {
         return this.employeeRepository.findById(id)
             .orElseThrow(() -> new EmployeeNotFoundException(id));
