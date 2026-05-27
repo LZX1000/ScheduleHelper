bbd7fea7be650257239ac615afc6764d0779730f
Xander White <xaw8720@g.rit.edu>
2026-05-27 16:01:32 -0400

    refactor: remove unused EmployeeNotFoundException constructor

--- server/src/main/java/com/schedulehelper/api/exception/EmployeeNotFoundException.java
+++ server/src/main/java/com/schedulehelper/api/exception/EmployeeNotFoundException.java
@@ -4,8 +4,4 @@ public class EmployeeNotFoundException extends ScheduleHelperException {
     public EmployeeNotFoundException(final Integer id) {
         super("Employee with id " + id + " not found.");
     }
-
-    public EmployeeNotFoundException(final String message) {
-        super(message);
-    }
 }
